f839d28eb1be8fd3bbd26afeb6776239a5b82553
moeenchanna <moin.channa@paysyslab.com>
2022-05-25 16:29:19 +0500

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+
+void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePge(),
+    );
+  }
+}
+
+class HomePge extends StatefulWidget {
+  const HomePge({Key? key}) : super(key: key);
+
+  @override
+  State<HomePge> createState() => _HomePgeState();
+}
+
+class _HomePgeState extends State<HomePge> {
+  @override
+  void initState() {
+    super.initState();
+
+    /// whenever your initialization is completed, remove the splash screen:
+    Future.delayed(const Duration(seconds: 5))
+        .then((value) => {FlutterNativeSplash.remove()});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.redAccent,
+        centerTitle: true,
+        title: const Text("Splash Screen"),
+      ),
+    );
+  }
+}
